761245c0a6463f69433b2ed8fa218ebd4817ea2d
Aoi <aoiaoi0412.1203@gmail.com>
2026-05-03 00:59:39 +0900

    fix: enhance debug alert with detailed information on lineSource and schoolData length

--- js/app.js
+++ js/app.js
@@ -95,7 +95,7 @@ window.onload = async function () {
 		const schoolEntry = (schoolData || []).find((s) => s && s.id === lineSource);
 		const channelId =
 			schoolEntry && schoolEntry.lineChannelId ? schoolEntry.lineChannelId : LINE_CHANNEL_ID;
-		alert("Debug channelId: " + channelId);
+		alert("Debug info - lineSource: " + lineSource + ", schoolData length: " + (schoolData ? schoolData.length : 0) + ", channelId: " + channelId);
 		window.location.href = buildLineAuthUrl(channelId);
 		return;
 	}
